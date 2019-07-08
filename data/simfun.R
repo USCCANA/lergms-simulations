78a53c93e1bbdd63427b7e71aad916bcd400af7c
George G. Vega Yon <g.vegayon@gmail.com>
2019-07-08 10:46:40 -0700

    Working on DGP

--- data/simfun.R
+++ data/simfun.R
@@ -10,29 +10,34 @@ simfun <- function(d, boot = FALSE) {
   prop <- unlist(prop, recursive = TRUE)
   prop <- sum(prop == 1, na.rm = TRUE)/sum(!is.na(prop))
   
-  if (min(prop, 1-prop) < .1) {
-    null <- d$par
-    null[] <- rep(NA, length(null))
-    return(
-      list(
-        coef    = null,
-        vcov    = matrix(NA, nrow = length(null), ncol = length(null),
-                      dimnames = list(names(null), c("2.5 %", "97.5 %"))),
-        balance = prop
-      )
-    )
-  }
+  #if (min(prop, 1-prop) < .1) {
+  #  null <- d$par
+  #  null[] <- rep(NA, length(null))
+  #  return(
+  #    list(
+  #      coef    = null,
+  #      vcov    = matrix(NA, nrow = length(null), ncol = length(null),
+  #                    dimnames = list(names(null), c("2.5 %", "97.5 %"))),
+  #      balance = prop
+  #    )
+  #  )
+  #}
     
   # Else we estimate the lergm
   estimates <- if (!boot) 
-    ergmito(nets ~ edges + mutual)
+    tryCatch(ergmito(nets ~ edges + mutual, ntries = 1L), error = function(e) e)
   else
     ergmito_boot(nets ~ edges + mutual, ncpus = 1L, R=1000)
   
+  if (inherits(estimates, "error"))
+    return(estimates)
+  
+
   list(
     coef    = coef(estimates),
     vcov    = confint(estimates),
-    balance = prop
+    balance = prop,
+    optim.out = estimates$optim.out
   )
     
     
--- simulations/02-various-sizes-3-5.R
+++ simulations/02-various-sizes-3-5.R
@@ -9,9 +9,12 @@ dat <- readRDS("simulations/dgp_3_5.rds")
 # Creating cluster
 opts_sluRm$set_chdir("/staging/ggv/")
 opts_sluRm$set_job_name("02-various-sizes-3-5")
-opts_sluRm$set_opts(account = "lc_pdt", partition="thomas", time="02:00:00", `mem-per-cpu` = "2G")
+opts_sluRm$set_opts(
+   # account = "lc_pdt",
+   # partition="thomas",
+   time="04:00:00", `mem-per-cpu` = "2G")
 
-job <- Slurm_lapply(dat, simfun, njobs = 40, mc.cores = 4L)
+job <- Slurm_lapply(dat, simfun, njobs = 10, mc.cores = 8L)
 ans <- Slurm_collect(job)
 
 saveRDS(ans, "simulations/02-various-sizes-3-5.rds", compress = FALSE)
