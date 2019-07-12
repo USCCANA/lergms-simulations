82b8128dd2c0cd737f7cee1343f50275df6855c7
George G. Vega Yon <g.vegayon@gmail.com>
2019-07-12 14:59:41 -0700

    Working on fitters

--- simulations/02-various-sizes-3-5.R
+++ simulations/02-various-sizes-3-5.R
@@ -1,9 +1,10 @@
 library(ergmito)
+library(ergm)
 library(parallel)
 library(sluRm)
 
 # Loading simulation function and data
-source("data/simfun.R")
+source("data/model_fitters.R")
 dat <- readRDS("simulations/dgp_3_5.rds")
 
 # Creating cluster
@@ -12,13 +13,13 @@ opts_sluRm$set_job_name("02-various-sizes-3-5")
 opts_sluRm$set_opts(
    account = "lc_dvc",
    partition="conti",
-   time="04:00:00", `mem-per-cpu` = "2G"
+   time="04:00:00", `mem-per-cpu` = "1G"
 )
 
 # Checking veb
 opts_sluRm$verbose_on()
 
-job <- Slurm_lapply(dat, simfun, njobs = 10, mc.cores = 8L, plan = "wait")
+job <- Slurm_lapply(dat, fitter, njobs = 200, mc.cores = 1L, plan = "wait")
 ans <- Slurm_collect(job)
 
 saveRDS(ans, "simulations/02-various-sizes-3-5.rds", compress = FALSE)
