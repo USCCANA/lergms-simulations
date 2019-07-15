141cc403a60f3a78c25eda94ea6b9698af666633
George G. Vega Yon <g.vegayon@gmail.com>
2019-07-15 09:29:45 -0700

    Adjusting simulations to accomodate ergm fitting

--- 00-run.sh
+++ 00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --job-name="00-dgp"
-#SBATCH --output="logs/00-dgp%A.out"
+#SBATCH --output="logs/00-dgp.out"
 #SBATCH --time="24:00:00"
 #SBATCH --mem-per-cpu=8G
 #SBATCH --partition=conti
