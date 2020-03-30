0c81a985c3cd1f689ac401f78f6dead26079d01d
George G. Vega Yon <g.vegayon@gmail.com>
2020-03-29 23:07:41 -0700

    Working on review

--- data/fitter.R
+++ data/fitter.R
@@ -10,7 +10,7 @@ fitter <- function(model, d, boot = FALSE) {
   model <- update.formula(model, nets ~ .)
   environment(model) <- environment()
 
-  time_ergmito <- system.time(ans_ergmito <- if (!boot) 
+  time_mle <- system.time(ans_mle <- if (!boot) 
     tryCatch(ergmito(model, ntries = 1L), error = function(e) e)
   else
     ergmito_boot(model, ncpus = 1L, R=1000)
@@ -21,45 +21,76 @@ fitter <- function(model, d, boot = FALSE) {
   model_bd <- update.formula(model, nets_bd ~ .)
   environment(model_bd) <- environment()
   
-  time_ergm <- system.time(ans_ergm <- tryCatch(ergm(
+  time_mcmle <- system.time(ans_mcmle <- tryCatch(ergm(
     model_bd,
     constraints = ~ blockdiag("block"),
     control     = control.ergm(
       # Default values equal to 1048
-      MCMC.samplesize = 2048L,
-      MCMC.interval   = 2048,
+      MCMC.samplesize = 1024L * 4L,
+      MCMC.interval   = 1024L * 4L,
+      MCMC.burnin     = 1024L * 8L,
       seed            = 1L
       )
     ), error = function(e) e))
   
-  if (inherits(ans_ergmito, "ergmito")) {
-    estimates_ergmito <- list(
-      coef       = coef(ans_ergmito),
-      ci         = confint(ans_ergmito),
-      vcov       = vcov(ans_ergmito),
-      status     = ans_ergmito$status,
-      ll         = logLik(ans_ergmito),
-      time       = time_ergmito
+  time_rm <- system.time(ans_rm <- tryCatch(ergm(
+    model_bd,
+    constraints = ~ blockdiag("block"),
+    control     = control.ergm(
+      # Default values equal to 1048
+      # MCMC.samplesize = 2048L,
+      # MCMC.interval   = 2048,
+      main.method     = "Robbins-Monro",
+      MCMC.samplesize = 1024L * 4L,
+      MCMC.interval   = 1024L * 4L,
+      MCMC.burnin     = 1024L * 8L,
+      seed            = 1L
     )
-  } else estimates_ergmito <- ans_ergmito
+  ), error = function(e) e))
+  
+  if (inherits(ans_mle, "ergmito")) {
+    estimates_mle <- list(
+      coef       = coef(ans_mle),
+      ci         = confint(ans_mle),
+      vcov       = vcov(ans_mle),
+      status     = ans_mle$status,
+      ll         = logLik(ans_mle),
+      time       = time_mle
+    )
+  } else estimates_mle <- ans_mle
 
-  if (inherits(ans_ergm, "ergm")) {
-    estimates_ergm <- list(
-      coef = coef(ans_ergm),
-      ci   = confint(ans_ergm),
-      vcov = vcov(ans_ergm),
+  if (inherits(ans_mcmle, "ergm")) {
+    estimates_mcmle <- list(
+      coef = coef(ans_mcmle),
+      ci   = confint(ans_mcmle),
+      vcov = vcov(ans_mcmle),
       degeneracy = any(is.infinite(
-        ergm.degeneracy(ans_ergm)$degeneracy.value)
+        ergm.degeneracy(ans_mcmle)$degeneracy.value)
       ),
-      ll   = logLik(ans_ergm),
-      time = time_ergm
+      ll   = logLik(ans_mcmle),
+      time = time_mcmle
     )
   } else
-    estimates_ergm <- ans_ergm
+    estimates_mcmle <- ans_mcmle
+  
+  if (inherits(ans_rm, "ergm")) {
+    estimates_ergm <- list(
+      coef = coef(ans_rm),
+      ci   = confint(ans_rm),
+      vcov = vcov(ans_rm),
+      degeneracy = any(is.infinite(
+        ergm.degeneracy(ans_rm)$degeneracy.value)
+      ),
+      ll   = logLik(ans_rm),
+      time = time_rm
+    )
+  } else
+    estimates_rm <- ans_rm
   
   list(
-    ergmito = estimates_ergmito,
-    ergm    = estimates_ergm
+    mle = estimates_mle,
+    mcmle    = estimates_mcmle,
+    rm = estimates_rm
   )
   
 }
